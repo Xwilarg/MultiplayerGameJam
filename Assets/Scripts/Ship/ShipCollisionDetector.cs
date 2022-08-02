d68fb455e7544905e3e08239d6d1d2549534412e
Xwilarg <xwilarg@protonmail.com>
2022-08-02 21:29:06 +0200

    Add trigger detection

--- Assets/Scripts/Ship/ShipCollisionDetector.cs
+++ Assets/Scripts/Ship/ShipCollisionDetector.cs
@@ -4,9 +4,27 @@ namespace MultiplayerGameJam.Ship
 {
     public class ShipCollisionDetector : MonoBehaviour
     {
+        private ShipController _controller;
+
+        private void Awake()
+        {
+            _controller = transform.parent.GetComponent<ShipController>();
+        }
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            transform.parent.GetComponent<ShipController>();
+            if (collision.CompareTag("Ship")) // Collide with another ship
+            {
+
+            }
+            else if (collision.CompareTag("Border")) // Collider with the border of the map, we can probably sink the ship
+            {
+
+            }
+            else // Any other item we can roll over
+            {
+                Destroy(collision.gameObject);
+            }
         }
     }
 }
