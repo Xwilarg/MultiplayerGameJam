0b0d1bd81fa4542c8664709839f48caa4fe24966
Xwilarg <xwilarg@protonmail.com>
2022-08-03 09:10:45 +0200

    Add other ship in collisions

--- Assets/Scripts/Ship/ShipCollisionDetector.cs
+++ Assets/Scripts/Ship/ShipCollisionDetector.cs
@@ -15,7 +15,7 @@ namespace MultiplayerGameJam.Ship
         {
             if (collision.CompareTag("Ship")) // Collide with another ship
             {
-
+                var other = collision.GetComponent<ShipController>();
             }
             else if (collision.CompareTag("Border")) // Collider with the border of the map, we can probably sink the ship
             {
